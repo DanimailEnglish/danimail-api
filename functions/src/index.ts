a7f07c0e0aff64b2b60b27971ae99e38b60d3995
Abner Qian <abner@playhouse.so>
2023-04-28 01:00:37 -0700

    Add function that adds read only fields to newly created users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,14 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const addProtectedFieldsToNewUser = functions.auth
+  .user()
+  .onCreate(async (user) => {
+    await admin.firestore().doc(`users/${user.uid}/protected/readOnly`).set({
+      createdAt: new Date(),
+      role: "student",
+      unusedVideos: 0,
+    });
+  });
